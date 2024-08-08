684a61488153c90bc2997e606e53123b1fcc457c
= <=>
2024-08-08 15:43:18 -0600

    initial commit

---
+++ wezterm.lua
@@ -0,0 +1,47 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+local config = {}
+
+config.keys = {
+	{
+		key = "h",
+		mods = "CTRL|ALT",
+		action = act.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "l",
+		mods = "CTRL|ALT",
+		action = act.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "k",
+		mods = "CTRL|ALT",
+		action = act.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "j",
+		mods = "CTRL|ALT",
+		action = act.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "-",
+		mods = "CTRL|ALT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "|",
+		mods = "CTRL|ALT",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+}
+
+for i = 1, 8 do
+	-- CTRL+ALT + number to activate that tab
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "CTRL|ALT",
+		action = act.ActivateTab(i - 1),
+	})
+end
+
+return config
